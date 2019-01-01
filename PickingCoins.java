ff652d083057a9175d05451951cd093decc88d9f
Kemparaju <kemparaju@outlook.com>
2019-01-01 06:42:43 +0530

    Sol to Picking Coins Problem

---
+++ PickingCoins.java
@@ -0,0 +1,19 @@
+public class PickingCoins {
+    public static void main(String[] args) {
+        int n=10, k=2;
+
+        if(n == 0) {
+            System.out.println("Bob");
+
+        } else {
+            int i = 1, drawCoins;
+            do {
+                drawCoins = 
+
+            }while(true);
+        }
+
+
+
+    }
+}
